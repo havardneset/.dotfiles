96b1355b9571975be67a4fbed0ac5b74fd8d2b5a
Håvard Neset <havard.neset@schibsted.com>
2024-11-23 17:34:04 +0100

    re-added nvim config

---
+++ .config/nvim/lua/keymaps.lua
@@ -0,0 +1,47 @@
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Search and replace word under the cursor.
+vim.keymap.set('n', '<Leader>r', [[:%s/\<<C-r><C-w>\>//g<Left><Left>]])
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- Prettify JSON
+vim.api.nvim_create_user_command('JsonFormat', function()
+  vim.cmd '%!jq'
+end, {})
+
+vim.keymap.set('n', '<leader>js', ':JsonFormat<CR>')
+
+-- Replace newlines
+vim.api.nvim_create_user_command('NewLines', function()
+  vim.cmd '%s/ 	/\r/g'
+end, {})
+
+vim.keymap.set('n', '<leader>nl', ':NewLines<CR>')
+
+-- Centralize screen after moving
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true })
