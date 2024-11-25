51239e81a7e3cba7ddf63c324f4bed7d68aa58fd
Håvard Neset <havard.neset@schibsted.com>
2024-11-25 12:38:35 +0100

    Added command for decoding JWT tokens

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -45,3 +45,32 @@ vim.keymap.set('n', '<leader>nl', ':NewLines<CR>')
 -- Centralize screen after moving
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true })
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true })
+
+-- decode JWT tokens
+vim.api.nvim_create_user_command('JwtDecodeBuffer', function()
+  -- Get the content of the entire buffer
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local jwt = table.concat(lines, '') -- Combine all lines into a single string
+
+  if jwt == '' then
+    print 'The buffer is empty or does not contain a valid JWT.'
+    return
+  end
+
+  -- Shell command to decode the JWT (both header and payload)
+  local cmd = string.format("echo '%s' | sed 's/\\./\\n/g' | base64 --decode | jq", jwt)
+  local handle = io.popen(cmd)
+  local result = handle:read '*a'
+  handle:close()
+
+  -- Display the result in a new buffer
+  if result and result ~= '' then
+    vim.cmd 'new' -- Open a new split window
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(result, '\n'))
+  else
+    print 'Failed to decode the JWT.'
+  end
+end, {})
+
+-- Mapping for <leader>jwt
+vim.keymap.set('n', '<leader>jwt', ':JwtDecodeBuffer<CR>', { noremap = true, silent = true })
