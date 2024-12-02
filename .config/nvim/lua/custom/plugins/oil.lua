d345afb2708a0d4732391653a086435682c64b17
Håvard Neset <havard.neset@schibsted.com>
2024-12-02 10:12:39 +0100

    Show hidden files in Oil

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -3,6 +3,16 @@ return {
     'stevearc/oil.nvim',
     opts = {
       default_file_explorer = true,
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name, bufnr)
+          return name == '..' or name == '.git'
+        end,
+      },
     },
+    init = function()
+      vim.api.nvim_set_keymap('n', '-', ':Oil<CR>', { noremap = true, silent = true })
+    end,
   },
 }
