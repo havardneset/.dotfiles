19b8e792ae926983c252bca01ed4d1382877e19e
Håvard Neset <havard.neset@schibsted.com>
2024-12-10 14:36:58 +0100

    Removed comment

--- .config/nvim/lua/custom/plugins/markdown.lua
+++ .config/nvim/lua/custom/plugins/markdown.lua
@@ -1,7 +1,7 @@
 return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you use the mini.nvim suite
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
