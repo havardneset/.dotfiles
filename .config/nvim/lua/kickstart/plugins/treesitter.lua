e7eb5a8e24c8784cc64e80183cc5596eba77eaac
Håvard Neset <havard.neset@schibsted.com>
2025-01-07 10:04:37 +0100

    Clean up LSP and treesitter configs

--- .config/nvim/lua/kickstart/plugins/treesitter.lua
+++ .config/nvim/lua/kickstart/plugins/treesitter.lua
@@ -1,8 +1,8 @@
 return {
-  { -- Highlight, edit, and navigate code
+  {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    main = 'nvim-treesitter.configs',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = {
@@ -21,6 +21,8 @@ return {
         'json',
         'go',
         'helm',
+        'typescript',
+        'javascript',
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
@@ -33,12 +35,6 @@ return {
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
