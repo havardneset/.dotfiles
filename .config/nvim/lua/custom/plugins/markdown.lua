d2976d8f6a69c49ac4db2933e32a8a196f9074d2
Håvard Neset <havard.neset@schibsted.com>
2025-01-13 08:23:55 +0100

    Enter insert mode after adding checkbox

--- .config/nvim/lua/custom/plugins/markdown.lua
+++ .config/nvim/lua/custom/plugins/markdown.lua
@@ -23,6 +23,14 @@ return {
         local line = vim.api.nvim_get_current_line()
         local new_line = '- [ ] ' .. line
         vim.api.nvim_set_current_line(new_line)
+
+        -- Move the cursor to the end of the current line
+        local cursor_pos = vim.api.nvim_win_get_cursor(0)
+        cursor_pos[2] = #new_line
+        vim.api.nvim_win_set_cursor(0, cursor_pos)
+
+        -- Enter insert mode
+        vim.cmd 'startinsert!'
       end, { desc = 'Add checkbox to current line' })
 
       vim.keymap.set('n', '<leader>ac', ':AddCheckbox<CR>', { desc = 'Add checkbox' })
