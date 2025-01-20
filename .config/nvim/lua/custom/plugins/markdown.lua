4f6425e7f48fd794071b7e7f508b1dd7b6521f93
Håvard Neset <havard.neset@schibsted.com>
2025-01-20 13:09:25 +0100

    Added ability to add checkboxes to the next line

--- .config/nvim/lua/custom/plugins/markdown.lua
+++ .config/nvim/lua/custom/plugins/markdown.lua
@@ -21,18 +21,26 @@ return {
 
       vim.api.nvim_create_user_command('AddCheckbox', function()
         local line = vim.api.nvim_get_current_line()
-        local new_line = '- [ ] ' .. line
-        vim.api.nvim_set_current_line(new_line)
 
-        -- Move the cursor to the end of the current line
-        local cursor_pos = vim.api.nvim_win_get_cursor(0)
-        cursor_pos[2] = #new_line
-        vim.api.nvim_win_set_cursor(0, cursor_pos)
+        -- Check if the current line already contains a checkbox
+        if line:match '%- %[.%]' then
+          -- Move to the next line and add a checkbox
+          local cursor_pos = vim.api.nvim_win_get_cursor(0)
+          vim.api.nvim_buf_set_lines(0, cursor_pos[1], cursor_pos[1], true, { '- [ ] ' })
+          vim.api.nvim_win_set_cursor(0, { cursor_pos[1] + 1, 0 })
+        else
+          -- Add a checkbox to the current line
+          local new_line = '- [ ] ' .. line
+          vim.api.nvim_set_current_line(new_line)
+          -- Move the cursor to the end of the current line
+          local cursor_pos = vim.api.nvim_win_get_cursor(0)
+          cursor_pos[2] = #new_line
+          vim.api.nvim_win_set_cursor(0, cursor_pos)
+        end
 
         -- Enter insert mode
         vim.cmd 'startinsert!'
-      end, { desc = 'Add checkbox to current line' })
-
+      end, { desc = 'Add checkbox to current line or next line if already present' })
       vim.keymap.set('n', '<leader>ca', ':AddCheckbox<CR>', { desc = 'Add checkbox' })
       vim.keymap.set('n', '<leader>cc', ':ToggleCheckbox<CR>', { desc = 'Toggle checkbox' })
     end,
