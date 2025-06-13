7f59eea67e42d56ef2998578c2cdb98691403de4
Håvard Neset <havard.neset@schibsted.com>
2025-06-13 14:25:41 +0200

    Disable the <CR> keymap in quickfix window

--- .config/nvim/lua/custom/plugins/flash.lua
+++ .config/nvim/lua/custom/plugins/flash.lua
@@ -4,8 +4,15 @@ return {
     event = 'VeryLazy',
     ---@type Flash.Config
     opts = {},
-    keys = {
-      { '<CR>', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash' },
-    },
+    init = function()
+      -- Create global mapping for <CR>
+      vim.keymap.set({ 'n', 'x', 'o' }, '<CR>', function() require('flash').jump() end, { desc = 'Flash' })
+
+      -- Disable the mapping in quickfix windows using an autocmd
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'qf',
+        callback = function() vim.keymap.set({ 'n', 'x', 'o' }, '<CR>', '<CR>', { buffer = true, desc = 'Default quickfix enter' }) end,
+      })
+    end,
   },
 }
