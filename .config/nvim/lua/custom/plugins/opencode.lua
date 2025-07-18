f6cd9a2617f6c20634ef273f51666e16fa62ef8d
Håvard Neset <havard.neset@schibsted.com>
2025-07-18 14:34:07 +0200

    Add visual mode specific opencode keymap

--- .config/nvim/lua/custom/plugins/opencode.lua
+++ .config/nvim/lua/custom/plugins/opencode.lua
@@ -13,7 +13,8 @@ return {
     -- opencode.nvim exposes a general, flexible API — customize it to your workflow!
     -- But here are some examples to get you started :)
     { '<leader>ot', function() require('opencode').toggle() end, desc = 'Toggle opencode', },
-    { '<leader>oa', function() require('opencode').ask() end, desc = 'Ask opencode', mode = { 'n', 'v' }, },
+    { '<leader>oa', function() require('opencode').ask() end, desc = 'Ask opencode', mode = { 'n' }, },
+    { '<leader>oa', function() require('opencode').ask('@selection ') end, desc = 'Ask opencode about selection', mode = { 'v' }, },
     { '<leader>oA', function() require('opencode').ask('@file ') end, desc = 'Ask opencode about current file', mode = { 'n', 'v' }, },
     { '<leader>on', function() require('opencode').command('/new') end, desc = 'New session', },
     { '<leader>oe', function() require('opencode').prompt('Explain @cursor and its context') end, desc = 'Explain code near cursor' },
@@ -21,7 +22,6 @@ return {
     { '<leader>of', function() require('opencode').prompt('Fix these @diagnostics') end, desc = 'Fix errors', },
     { '<leader>oo', function() require('opencode').prompt('Optimize @selection for performance and readability') end, desc = 'Optimize selection', mode = 'v', },
     { '<leader>od', function() require('opencode').prompt('Add documentation comments for @selection') end, desc = 'Document selection', mode = 'v', },
-    { '<leader>ot', function() require('opencode').prompt('Add tests for @selection') end, desc = 'Test selection', mode = 'v', },
   },
   },
 }
