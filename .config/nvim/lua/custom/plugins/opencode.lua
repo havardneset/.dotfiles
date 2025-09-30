a33d8745addce48ec0f1ec6bf7711b187948cf77
Håvard Neset <havard.neset@schibsted.com>
2025-09-30 11:22:42 +0200

    Try out another opencode plugin

--- .config/nvim/lua/custom/plugins/opencode.lua
+++ .config/nvim/lua/custom/plugins/opencode.lua
@@ -1,27 +1,26 @@
 return {
   {
-    'NickvanDyke/opencode.nvim',
+    'sudo-tee/opencode.nvim',
+    config = function() require('opencode').setup {} end,
     dependencies = {
-      'folke/snacks.nvim',
+      'nvim-lua/plenary.nvim',
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          anti_conceal = { enabled = false },
+          file_types = { 'markdown', 'opencode_output' },
+        },
+        ft = { 'markdown', 'Avante', 'copilot-chat', 'opencode_output' },
+      },
+      -- Optional, for file mentions and commands completion, pick only one
+      -- 'saghen/blink.cmp',
+      'hrsh7th/nvim-cmp',
+
+      -- Optional, for file mentions picker, pick only one
+      -- 'folke/snacks.nvim',
+      'nvim-telescope/telescope.nvim',
+      -- 'ibhagwan/fzf-lua',
+      -- 'nvim_mini/mini.nvim',
     },
-    ---@type opencode.Config
-    opts = {
-      -- Your configuration, if any
-    },
-  -- stylua: ignore
-  keys = {
-    -- opencode.nvim exposes a general, flexible API — customize it to your workflow!
-    -- But here are some examples to get you started :)
-    { '<leader>ot', function() require('opencode').toggle() end, desc = 'Toggle opencode', },
-    { '<leader>oa', function() require('opencode').ask() end, desc = 'Ask opencode', mode = { 'n' }, },
-    { '<leader>oa', function() require('opencode').ask('@selection ') end, desc = 'Ask opencode about selection', mode = { 'v' }, },
-    { '<leader>oA', function() require('opencode').ask('@file ') end, desc = 'Ask opencode about current file', mode = { 'n', 'v' }, },
-    { '<leader>on', function() require('opencode').command('/new') end, desc = 'New session', },
-    { '<leader>oe', function() require('opencode').prompt('Explain @cursor and its context') end, desc = 'Explain code near cursor' },
-    { '<leader>or', function() require('opencode').prompt('Review @file for correctness and readability') end, desc = 'Review file', },
-    { '<leader>of', function() require('opencode').prompt('Fix these @diagnostics') end, desc = 'Fix errors', },
-    { '<leader>oo', function() require('opencode').prompt('Optimize @selection for performance and readability') end, desc = 'Optimize selection', mode = 'v', },
-    { '<leader>od', function() require('opencode').prompt('Add documentation comments for @selection') end, desc = 'Document selection', mode = 'v', },
-  },
   },
 }
